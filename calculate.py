1941df9fe5ac0cdf1b4958e1cee8454b3c64c878
Arnav Jain <90660802+ArnoUUU@users.noreply.github.com>
2022-03-13 12:06:19 -0700

    added moveright,using the right switch helper function, also changed push a bit completely rehought the architechure of this, seems more efficient now

--- calculate.py
+++ calculate.py
@@ -1,17 +1,32 @@
-def push(matrix, d):
-    if d == "left":
-        while True:
-            try: 
-                ind = matrix.index("")
-                try:
-                    if matrix[ind+1] != "":
-                        matrix[ind] = matrix[ind+1]
-                        matrix[ind+1] = "gibberish"
-                except Exception:
-                    matrix[ind] = "gibberish"
+def moveleft(matrix):
+    return push(matrix)
+def moveright(matrix):
+    matrix = rightswitch(matrix)
+    matrix = push(matrix)
+    matrix = rightswitch(matrix)
+    return matrix
+def rightswitch(matrix):
+    matrix = switchvals(matrix, 0, 3)
+    matrix = switchvals(matrix, 1, 2)
+    matrix = switchvals(matrix, 4, 7)
+    matrix = switchvals(matrix, 5, 6)
+    matrix = switchvals(matrix, 8, 11)
+    matrix = switchvals(matrix, 9, 10)
+    matrix = switchvals(matrix, 12, 15)
+    matrix = switchvals(matrix, 13, 14)
+def push(matrix):
+    while True:
+        try: 
+            ind = matrix.index("")
+            try:
+                if matrix[ind+1] != "":
+                    matrix[ind] = matrix[ind+1]
+                    matrix[ind+1] = "gibberish"
             except Exception:
-                for i in range(matrix.count("gibberish")):
-                    ind = matrix.index("gibberish")
-                    matrix[ind] = ""
-        return matrix
+                matrix[ind] = "gibberish"
+        except Exception:
+            for i in range(matrix.count("gibberish")):
+                ind = matrix.index("gibberish")
+                matrix[ind] = ""
+    return matrix
 
