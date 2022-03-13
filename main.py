7a1e33163c5184f14046f9474d588c4011e07451
Arnav Jain <90660802+ArnoUUU@users.noreply.github.com>
2022-03-12 21:49:33 -0800

    feat: added a functio to add a two or four into the matrix Also made the matrix into a list to simplify things

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-matrix = [['','','',''],['','','',''],['','','',''],['','','','']]
+import random
+matrix = ["","","","","","","","","","","","","","","",""]
+def addint(matrix):
+    oneortwo = random.randint(1,2)
+    twoorfour = oneortwo*2
+    rando = 0
+    for i in range(matrix.count("")):
+        rando += 1
+        ind = matrix.index("")
+        matrix[ind] = str(rando)
+    spot = random.randint(1, rando)
+    ind = matrix.index(str(spot))
+    matrix[ind] = twoorfour
+    for i in range(len(matrix)):
+        if type(matrix[i])==str:
+            matrix[i] = ""
+    return matrix    
