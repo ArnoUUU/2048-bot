37f7c593d0b11800ce769a41f0f38815a51bd4f0
Arnav Jain <90660802+ArnoUUU@users.noreply.github.com>
2022-03-13 12:18:19 -0700

    feat: implemented moveup functions with its helper upswitch function

--- calculate.py
+++ calculate.py
@@ -1,24 +1,4 @@
-def switchvals(matrix, a,b):
-    matrix[a] = matrix[a] + matrix[b]
-    matrix[b] = matrix[a] - matrix[b]
-    matrix[a] = matrix[a] - matrix[b]
-    return matrix
-def moveleft(matrix):
-    return push(matrix)
-def moveright(matrix):
-    matrix = rightswitch(matrix)
-    matrix = push(matrix)
-    matrix = rightswitch(matrix)
-    return matrix
-def rightswitch(matrix):
-    matrix = switchvals(matrix, 0, 3)
-    matrix = switchvals(matrix, 1, 2)
-    matrix = switchvals(matrix, 4, 7)
-    matrix = switchvals(matrix, 5, 6)
-    matrix = switchvals(matrix, 8, 11)
-    matrix = switchvals(matrix, 9, 10)
-    matrix = switchvals(matrix, 12, 15)
-    matrix = switchvals(matrix, 13, 14)
+
 def push(matrix):
     while True:
         try: 
@@ -34,4 +14,36 @@ def push(matrix):
                 ind = matrix.index("gibberish")
                 matrix[ind] = ""
     return matrix
-
+def switchvals(matrix, a,b):
+    matrix[a] = matrix[a] + matrix[b]
+    matrix[b] = matrix[a] - matrix[b]
+    matrix[a] = matrix[a] - matrix[b]
+    return matrix
+def rightswitch(matrix):
+    matrix = switchvals(matrix, 0, 3)
+    matrix = switchvals(matrix, 1, 2)
+    matrix = switchvals(matrix, 4, 7)
+    matrix = switchvals(matrix, 5, 6)
+    matrix = switchvals(matrix, 8, 11)
+    matrix = switchvals(matrix, 9, 10)
+    matrix = switchvals(matrix, 12, 15)
+    matrix = switchvals(matrix, 13, 14)
+def upswitch(matrix):
+    matrix = switchvals(1,4)
+    matrix = switchvals(2,8)
+    matrix = switchvals(3,12)
+    matrix = switchvals(6,9)
+    matrix = switchvals(7,13)
+    matrix = switchvals(11,14)
+def moveleft(matrix):
+    return push(matrix)
+def moveright(matrix):
+    matrix = rightswitch(matrix)
+    matrix = push(matrix)
+    matrix = rightswitch(matrix)
+    return matrix
+def moveup(matrix):
+    matrix = upswitch(matrix)
+    matrix = push(matrix)
+    matrix = upswitch(matrix)
+    return matrix
