90e0547e85eac1ba4d90a11c8d398d3498654f89
Arnav Jain <90660802+ArnoUUU@users.noreply.github.com>
2022-03-14 22:00:11 -0700

    feat: wrote simple code in main, untested

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-import random
+import calculate
 import addint
+from calculate import compareoptions
 matrix = ["","","","","","","","","","","","","","","",""]
+while matrix.count("2048")==0:
+    matrix = addint(matrix)
+    matrix = compareoptions(matrix)
+print(matrix)
