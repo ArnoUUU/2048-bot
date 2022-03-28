d224e2e6ec1322f3c59d3e2315626883b8dd8c4f
Arnav Jain <90660802+ArnoUUU@users.noreply.github.com>
2022-03-28 11:28:41 -0700

    fix: the compareoptions function should actually work now

--- calculate.py
+++ calculate.py
@@ -7,8 +7,5 @@ def compareoptions(matrix):
     possible_moves =[left.count(""),left,right.count(""),right,up.count(""),up,down.count(""),down]
     result = [left.count(""),right.count(""), down.count(""), up.count("")]
     result.sort()
-    for i in range(16):
-        num=possible_moves.count(i)
-        if num !=0:
-            x=possible_moves.index(num)
-            return possible_moves[x+1]
+    x=possible_moves.index(result[0])
+    return possible_moves[x+1]
