651d4912aeda46e9b0b14a00151d60a748569f78
Arnav Jain <90660802+ArnoUUU@users.noreply.github.com>
2022-03-29 09:23:14 -0700

    feat: constantly reruns the dumb logic again and again until it hits 2048

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import addint
 from calculate import compareoptions
-f = open("logs.txt","w")
-f.write("")
-f.close()
 matrix = ["","","","","","","","","","","","","","","",""]
-while matrix.count("2048")==0:
-  try:
-    matrix = addint.addint(matrix)
-    f = open("logs.txt", "a")
-    f.write(str(matrix))
-    f.close()
-    matrix = compareoptions(matrix)
-  except Exception:
-    print(matrix)
-    break
+
+while matrix.count(2048)==0:
+  f = open("logs.txt","w")
+  f.write("")
+  f.close()
+  matrix = ["","","","","","","","","","","","","","","",""]
+  while matrix.count(2048)==0:
+    try:
+      matrix = addint.addint(matrix)
+      f = open("logs.txt", "a")
+      f.write(str(matrix))
+      f.close()
+      matrix = compareoptions(matrix)
+    except Exception:
+      print(matrix)
+      break
 print("THE END")
