e2ebdc7329aeeb873e62e2380772d756b704d9ab
Arnav Jain <90660802+ArnoUUU@users.noreply.github.com>
2022-03-29 08:55:14 -0700

    feat: main now uses logs.txt

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-import calculate
 import addint
 from calculate import compareoptions
+f = open("logs.txt","w")
+f.write("")
+f.close()
 matrix = ["","","","","","","","","","","","","","","",""]
 while matrix.count("2048")==0:
-    matrix = addint(matrix)
+  try:
+    matrix = addint.addint(matrix)
+    f = open("logs.txt", "a")
+    f.write(str(matrix))
+    f.close()
     matrix = compareoptions(matrix)
-print(matrix)
+  except Exception:
+    print(matrix)
+    break
+print("THE END")
