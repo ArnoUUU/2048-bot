c9d485170d7ef1834514ca06de3b3d1d5a612796
Arnav Jain <90660802+ArnoUUU@users.noreply.github.com>
2022-04-05 10:59:33 -0700

    docs: added comments and refactor: renamed rotateright to rotateclockwise

--- options.py
+++ options.py
@@ -1,3 +1,4 @@
+#it combines like terms (when you make a move in 2048, it combines like numbers in a specific order)
 def addsame(submatrix):
 	if submatrix[0]==submatrix[1]:      
 		submatrix=[submatrix[0]+submatrix[1], submatrix[2],submatrix[3],""]
@@ -6,6 +7,7 @@ def addsame(submatrix):
 	if submatrix[2]==submatrix[3]:
 		submatrix=[submatrix[0],submatrix[1], submatrix[2]+submatrix[3],""]
 	return submatrix
+#this is where all the computation happens when you moveleft. (the matrix will be ordered in a way )
 def combine(matrix):
 	submatrix1=[matrix[0],matrix[1],matrix[2],matrix[3]]
 	submatrix2=[matrix[4],matrix[5],matrix[6],matrix[7]]
@@ -20,6 +22,7 @@ def combine(matrix):
 	submatrix3=addsame(submatrix3)
 	submatrix4=addsame(submatrix4)
 	return [submatrix1[0],submatrix1[1],submatrix1[2],submatrix1[3],submatrix2[0],submatrix2[1],submatrix2[2],submatrix2[3],submatrix3[0],submatrix3[1],submatrix3[2],submatrix3[3],submatrix4[0],submatrix4[1],submatrix4[2],submatrix4[3]]
+#this pop and appends all fo teh null strings in the submatrix
 def push(submatrix):
 	matrixnum = submatrix.count("")
 	for i in range(matrixnum):
@@ -28,14 +31,17 @@ def push(submatrix):
 	for i in range(matrixnum):
 		submatrix.append("")
 	return submatrix
-def rotateright(matrix):
+#rotates the matrix 90 degrees clockwise
+def rotateclockwise(matrix):
 	return [matrix[12],matrix[8],matrix[4],matrix[0],matrix[13],matrix[9],matrix[5],matrix[1],matrix[14],matrix[10],matrix[6],matrix[2],matrix[15],matrix[11],matrix[7],matrix[3]]
+# switches the values of two indexes in a matrix
 def switchvals(matrix, a,b):
 	c = matrix[a]
 	d = matrix[b]
 	matrix[a] = d
 	matrix[b] = c
 	return matrix
+#prepares a rightmoved matrix to be combined
 def rightswitch(matrix):
 	matrix = switchvals(matrix, 0, 3)
 	matrix = switchvals(matrix, 1, 2)
@@ -46,6 +52,7 @@ def rightswitch(matrix):
 	matrix = switchvals(matrix, 12, 15)
 	matrix = switchvals(matrix, 13, 14)
 	return matrix
+#prepares a upmoved matrix to be combined
 def upswitch(matrix):
 	matrix = switchvals(matrix,1,4)
 	matrix = switchvals(matrix,2,8)
@@ -54,22 +61,26 @@ def upswitch(matrix):
 	matrix = switchvals(matrix,7,13)
 	matrix = switchvals(matrix,11,14)
 	return matrix
+#similar to pressing the left arrow key when playing 2048
 def moveleft(matrix):
 	return combine(matrix)
+#similar to pressing the right arrow key when playing 2048
 def moveright(matrix):
 	matrix = rightswitch(matrix)  
 	matrix = combine(matrix)
 	matrix = rightswitch(matrix)
 	return matrix
+#similar to pressing the up arrow key when playing 2048
 def moveup(matrix):
 	matrix = upswitch(matrix)
 	matrix = combine(matrix)
 	matrix = upswitch(matrix)
 	return matrix
+#similar to pressing the down arrow key when playing 2048
 def movedown(matrix):
-	matrix = rotateright(matrix)
+	matrix = rotateclockwise(matrix)
 	matrix = combine(matrix)
-	matrix = rotateright(matrix)
-	matrix = rotateright(matrix)
-	matrix = rotateright(matrix)
+	matrix = rotateclockwise(matrix)
+	matrix = rotateclockwise(matrix)
+	matrix = rotateclockwise(matrix)
 	return matrix
