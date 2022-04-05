ae85f82b83640ff5c8352e5019ddae5755a728eb
Arnav Jain <90660802+ArnoUUU@users.noreply.github.com>
2022-04-05 10:20:20 -0700

    organized order of functions in options and made a convention for tabs (2 spaces)

--- addint.py
+++ addint.py
@@ -1,16 +1,16 @@
 import random
 def addint(matrix):
-    oneortwo = random.randint(1,2)
-    twoorfour = oneortwo*2
-    rando = 0
-    for i in range(matrix.count("")):
-        rando += 1
-        ind = matrix.index("")
-        matrix[ind] = str(rando)
-    spot = random.randint(1, rando)
-    ind = matrix.index(str(spot))
-    matrix[ind] = twoorfour
-    for i in range(len(matrix)):
-        if type(matrix[i])==str:
-            matrix[i] = ""
-    return matrix    
+	oneortwo = random.randint(1,2)
+	twoorfour = oneortwo*2
+	rando = 0
+	for i in range(matrix.count("")):
+		rando += 1
+		ind = matrix.index("")
+		matrix[ind] = str(rando)
+	spot = random.randint(1, rando)
+	ind = matrix.index(str(spot))
+	matrix[ind] = twoorfour
+	for i in range(len(matrix)):
+		if type(matrix[i])==str:
+			matrix[i] = ""
+	return matrix    
