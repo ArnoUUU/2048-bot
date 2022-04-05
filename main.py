9c7ca36652c203dfea34166a9a427b12c4a30898
Arnav Jain <90660802+ArnoUUU@users.noreply.github.com>
2022-04-05 13:51:01 -0700

    subfeat: made variables for input

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import addint
 from calculate import compareoptions
 matrix = ["","","","","","","","","","","","","","","",""]
-
+logs = bool(input("Do you want to enable logs (this may take up a lot of space) for the failed tries? (type True if yes and False if no) Capitalization and spelling matter"))
+depth = int(input("What depth do you want me to calculate in? (positive integers only)"))
 while matrix.count(2048)==0:
   f = open("logs.txt","w")
   f.write("")
