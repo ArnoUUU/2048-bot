fdb5575be3becdf2c7ec852e6897ffba40b592d0
Arnav Jain <90660802+ArnoUUU@users.noreply.github.com>
2022-04-06 08:28:15 -0700

    chore: made logs say the next move is, and made teh matrix look 2d :3

--- main.py
+++ main.py
@@ -19,7 +19,13 @@ while matrix.count(2048)==0:
     try:
       matrix = addint.addint(matrix)
       f = open("logs.txt", "a")
-      f.write(str(matrix))
+      f.write("Next move is: ")
+      f.write(str(matrix[0],matrix[1],matrix[2],matrix[3]))
+      f.write(str(matrix[4],matrix[5],matrix[6],matrix[7]))
+      f.write(str(matrix[8],matrix[9],matrix[10],matrix[11]))
+      f.write(str(matrix[12],matrix[13],matrix[14],matrix[15]))
+      f.write("")
+      f.write("")
       f.close()
       matrix = compareoptions(matrix,depth)
     except Exception:
