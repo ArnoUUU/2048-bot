8a3fa673feef093824d287b474aefeafc3071649
Arnav Jain <90660802+ArnoUUU@users.noreply.github.com>
2022-04-26 08:16:59 -0700

    refactor: convertered bots.py to multiple functions which can be called by the new main.py

--- bot.py
+++ bot.py
@@ -1,34 +1,35 @@
 import addint
 from calculate import compareoptions
-matrix = ["","","","","","","","","","","","","","","",""]
-gamenum = 0
-logs = bool(input("Do you want to enable logs (this may take up a lot of space) for the failed tries? (type True if yes and False if no) Capitalization and spelling matter"))
-depth = int(input("What depth do you want me to calculate in? (positive integers only)"))
-if logs:
-  logs = "a"
-else:
-  logs = "w"
-#while matrix doesnt have 2048 empty logs and try again from scratch
-while matrix.count(2048)==0:
+def initiate(logs):
+  matrix = ["","","","","","","","","","","","","","","",""]
+  depth = 3
+  gamenum = 0
+  if logs:
+    logs = "a"
+  else:
+    logs = "w"
+  return matrix, depth, gamenum
+def after_move(matrix,logs):
+  matrix = addint.addint(matrix)
+  f = open("logs.txt", logs)
+  f.write("Next move is: "+"\n")
+  f.write(str(matrix))
+  f.write("\n")
+  f.write("\n")
+  f.close()
+#while matrix doesnt have 2048, calculate the best move and do it
+def iterate_game(matrix,logs,depth):
+  while matrix.count(2048)==0:
+    try:
+      matrix = after_move(matrix,logs)
+      matrix = compareoptions(matrix,depth)
+    except Exception:
+      return matrix
+  return matrix
+def new_game(logs,gamenum):
   gamenum += 1
   f = open("logs.txt", logs)
   f.write("")
   f.write("Game #"+str(gamenum)+"\n")
   f.close()
-  matrix = ["","","","","","","","","","","","","","","",""]
-  #while matrix doesnt have 2048, calculate the best move and do it
-  while matrix.count(2048)==0:
-    try:
-      matrix = addint.addint(matrix)
-      f = open("logs.txt", "a")
-      f.write("Next move is: "+"\n")
-      f.write(str(matrix))
-      f.write("\n")
-      f.write("\n")
-      f.close()
-      matrix = compareoptions(matrix,depth)
-    except Exception:
-      print(matrix)
-      break
-#when you are done print the end to show how lucky they are
-print("THE END")
+  return ["","","","","","","","","","","","","","","",""]
--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import pygame
+
+TILE_WIDTH = 64
+TILE_HEIGHT = 64
+
+WIN_WIDTH = 350
+WIN_HEIGHT = 350
+
+WIN = pygame.display.setmode(())
+
+clock = pyagem.time.Clock()
+
+run = True
+while run:
+    clock.tick()
+    for event in pygame.event.get():
+        if event == pygame.quit:
+            run = False
