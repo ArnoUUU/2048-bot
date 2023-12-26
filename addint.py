8744a93b53a01a94e4e3d4947d0b55151d5d1516
ArnoUUU <arnav.jain@outlook.com>
2023-12-26 12:42:56 -0800

    simplified randadd

--- addint.py
+++ addint.py
@@ -1,17 +1,12 @@
 import random
 # adds a 2 or a 4 in a random spot in the "matrix"
 def randadd(matrix):
+	nulls = []
 	oneortwo = random.randint(1,2)
-	twoorfour = oneortwo*2
-	randomax = 0
-	for i in range(matrix.count("")):
-		randomax += 1
-		ind = matrix.index("")
-		matrix[ind] = str(randomax)
-	spot = random.randint(1, randomax)
-	ind = matrix.index(str(spot))
-	matrix[ind] = twoorfour
-	for i in range(len(matrix)):
-		if type(matrix[i])==str:
-			matrix[i] = ""
+	randint = oneortwo*2
+	for i in range(15):
+		if matrix[i]==None:
+			nulls.append(i)
+	spot = random.randint(0, len(nulls)-1)
+	matrix[nulls[spot]] = randint
 	return matrix    
