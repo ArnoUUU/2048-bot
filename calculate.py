5f5c53fdd227bbba06b094217b64eacc7b6362af
ArnoUUU <arnav.jain@outlook.com>
2023-12-26 12:43:25 -0800

    Bugfixing!

--- calculate.py
+++ calculate.py
@@ -1,65 +1,32 @@
-import options
+
+def search(root,d):
+    legals = root.generateLegalMoves()
+    scores =[]
+    for move in legals:
+        scores.append(minimax(move,False,d-1))
+    return (legals[scores.index(max(scores))])
 
 
-def nulls(matrix):
-    nullist = []
-    for i in range(15):
-        if matrix[i] == "":
-            nullist.append(i)
-    return nullist
-
-
-def search(Node root, d):
-    left = options.moveleft(GameState)
-    right = options.moveright(GameState)
-    up = options.moveup(GameState)
-    down = options.movedown(GameState)
-    root.AddChild(left, )
-# uses xX! RECURSION !Xx to determine the move that will have the most null strings
-def recursor(GameState, d):
-    left = options.moveleft(GameState)
-    right = options.moveright(GameState)
-    up = options.moveup(GameState)
-    down = options.movedown(GameState)
+def minimax(root, human, d):
     if d == 0:
-        return evaluator(GameState.matrix)
-    null = nulls(GameState.matrix)
-    for i in range(len(null)):
-        GameState.matrix[null[i]] = 2
-        left2 = recursor(left, d - 1)
-        right2 = recursor(right, d - 1)
-        up2 = recursor(up, d - 1)
-        down2 = recursor(down, d - 1)
-        GameState.matrix[null[i]] = 4
-        left4 = recursor(left, d - 1)
-        right4 = recursor(right, d - 1)
-        up4 = recursor(up, d - 1)
-        down4 = recursor(down, d - 1)
-    avgs = [
-        	(left2.count("") + left4.count("")) / 2,
-        "l",
-        (right2.count("") + right4.count("")) / 2,
-        "r",
-        (up2.count("") + up4.count("")) / 2,
-        "u",
-        (down2.count("") + down4.count("")) / 2,
-        "d",
-    ]
-    result = [
-        (left2.count("") + left4.count("")) / 2,
-        (right2.count("") + right4.count("")) / 2,
-        (up2.count("") + up4.count("")) / 2,
-        (down2.count("") + down4.count("")) / 2,
-    ]
-    result.sort()
-    best = avgs[2 * avgs.index(result[3]) + 1]
-    if best == "l":
-        return left
-    if best == "r":
-        return right
-    if best == "u":
-        return up
-    if best == "p":
-        return down
-def evaluator():
-    return 
+        if root.generateLegalMoves() == []:
+            return 0
+        return expectiEvaluation(root)
+    legals = root.generateLegalMoves()
+    scores = []
+    for move in legals:
+        scores.append(minimax(move, not human, d-1))
+    if human:
+        return max(scores) 
+    else:
+        return min(scores)
+
+
+def evaluation(gamestate):
+    return gamestate.score
+
+def expectiEvaluation(gamestate):
+    score = gamestate.score
+    for i in range(3):
+        score += 2**i*gamestate.matrix[i]
+    return score
